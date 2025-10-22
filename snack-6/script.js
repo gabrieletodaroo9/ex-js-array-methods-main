e594f3e1a353087b4de95ba4a23a664caaa5c6b6
gabrieletodaroo9 <gabrieletodaroo9@gmail.com>
2025-10-22 16:41:22 +0200

    snack-6 completed

--- snack-6/script.js
+++ snack-6/script.js
@@ -12,3 +12,44 @@ const zucchine = [
 ];
 
 // A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+
+// FOR EACH 
+
+const longZucchine =[]
+const restZucchine = []
+
+zucchine.forEach(zucchina => {
+if ( zucchina.length > 15 ) {
+longZucchine.push(zucchina)
+} else{
+  restZucchine.push(zucchina)
+}
+})
+console.log(longZucchine, restZucchine);
+
+
+// FOR 
+
+const longZucchine2 = []
+const restZucchine2 = []
+
+for( i = 0; i < zucchine.length; i++) {
+  const zucchina = zucchine[i]
+  if ( zucchina.length > 15 ){
+    longZucchine2.push(zucchina)
+  }else {
+    restZucchine2.push(zucchina)
+  }
+}
+
+console.log(longZucchine2, restZucchine2);
+
+// FILTER
+
+const longZucchine3 = zucchine.filter(zucchina =>{
+  return zucchina.length > 15
+})
+const restZucchine3 = zucchine.filter(zucchina => {
+  return zucchina.length <= 15
+})
+console.log(longZucchine3,restZucchine3);
