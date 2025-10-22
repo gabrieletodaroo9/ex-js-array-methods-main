05e2e58f7a9e01d65401803333bba83c855b57fb
gabrieletodaroo9 <gabrieletodaroo9@gmail.com>
2025-10-22 15:46:01 +0200

    snack-1 completed

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,17 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+// CICLO FOR EACH
+
+names.forEach(name => {
+    console.log(name)
+    
+})
+
+// CICLO FOR
+
+for (i = 0; i < names.length; i++ ) {
+   console.log(names[i]);
+    
+}
