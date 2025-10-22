90b97c38b8b1d8e21041d160ae2c1afb4690fe79
gabrieletodaroo9 <gabrieletodaroo9@gmail.com>
2025-10-22 16:54:51 +0200

    snack-7 completed

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,34 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+// FOR EACH
+
+let studentFound = {}
+
+students.forEach(student => {
+if (student.id=== 2){
+  studentFound = student
+}
+})
+console.log(studentFound);
+
+
+// FOR 
+
+let studentFound2 = {}
+
+for ( let i = 0; i < students.length; i++){
+  const student = students[i]
+  if (student.id === 2 ){
+    studentFound2 = student 
+  }
+}
+console.log(studentFound2);
+
+// FIND
+
+const studentFound3 = students.find(student => {
+return student.id === 2
+})
+console.log(studentFound3);
