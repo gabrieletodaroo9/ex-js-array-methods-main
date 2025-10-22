e838b385cba2fab1b57d1b838077c13cd305ff02
gabrieletodaroo9 <gabrieletodaroo9@gmail.com>
2025-10-22 16:05:57 +0200

    snack-3 completed

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,32 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+// FOR EACH
+
+newArray=[]
+
+numbers.forEach( num => {
+    const numPlusOne = ++num
+    newArray.push(numPlusOne)
+    // console.log(numPlusOne);
+})
+console.log(newArray);
+
+// FOR
+
+newArray2 = []
+
+for( let i = 0;i < numbers.length;i++){
+    number = numbers[i]
+    newArray2.push(++number)
+}
+console.log(newArray2);
+
+
+// MAP
+
+const newArray3 = numbers.map(num=>{
+    return ++num
+})
+console.log(newArray3);
